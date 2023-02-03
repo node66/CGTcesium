cf57ff354365a184e73c2f134e048b2ef2bcb283
Vitaliy Shumikhin <vitaliy.shumikhin@cgtribe.com>
2023-02-03 16:23:31 +0400

    corrections06

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import "bootstrap";
 
-import { Viewer } from "cesium";
+import * as Cesium from "cesium";
+import {Viewer} from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import "../src/css/main.css"
-import * as Cesium from "cesium";
-import {addRow} from "./js/table";
+import {addRow, id} from "./js/table";
 
 export const viewer = new Viewer('cesiumContainer');
 const ellipsoid = viewer.scene.globe.ellipsoid;
@@ -13,49 +13,59 @@ const strToFloat = (str) => {
     return parseFloat(str.replace(",", "."));
 }
 
+
 const fileInput = document.getElementById("geojson-file-upload");
 fileInput.onchange = () => {
     const files = fileInput.files;
 
-    Object.keys(files).forEach( i => {
+    Object.keys(files).forEach(i => {
         const file = files[i];
         const reader = new FileReader();
         reader.readAsText(file);
         reader.onload = (e) => {
             Cesium.GeoJsonDataSource.load(JSON.parse(e.target.result))
                 .then(dataSource => {
+                    dataSource.name = file.name;
+                    if (viewer.dataSources.contains(dataSource))
+                        return;
+
                     addRow(file.name);
                     viewer.dataSources.add(dataSource)
-                        .then(r => r.entities.values.forEach(entity => {
-                            entity.billboard = undefined
+                        .then(r => {
+                            r.entities.values.forEach(entity => {
+                                entity.billboard = undefined
 
-                            const downrange = strToFloat(String(entity.properties['Downrange [Ra]'])) * 100;
+                                const cartographic = Cesium.Cartographic.fromCartesian(
+                                    entity.position.getValue(Cesium.JulianDate.now())
+                                )
 
-                            const cartographic = Cesium.Cartographic.fromCartesian(
-                                entity.position.getValue(Cesium.JulianDate.now())
-                            )
+                                cartographic.height = strToFloat(String(entity.properties.Altitude));
+                                entity.position = Cesium.Cartesian3.fromDegrees(
+                                    cartographic.longitude * Cesium.Math.DEGREES_PER_RADIAN,
+                                    cartographic.latitude * Cesium.Math.DEGREES_PER_RADIAN,
+                                    cartographic.height * Cesium.Math.DEGREES_PER_RADIAN,
+                                    ellipsoid,
+                                )
 
-                            cartographic.height = strToFloat(String(entity.properties.Altitude));
-                            entity.position = Cesium.Cartesian3.fromDegrees(
-                                cartographic.longitude * Cesium.Math.DEGREES_PER_RADIAN,
-                                cartographic.latitude * Cesium.Math.DEGREES_PER_RADIAN,
-                                cartographic.height * Cesium.Math.DEGREES_PER_RADIAN,
-                                ellipsoid,
-                            )
+                                const colorCallback = new Cesium.CallbackProperty(
+                                    () => {
+                                            return Cesium.Color.fromRandom()
+                                    }, false);
 
-                            entity.ellipsoid = new Cesium.EllipsoidGraphics({
-                                radii: new Cesium.Cartesian3(150000.0, 150000.0, 150000.0),
-                                material: new Cesium.Color(1, 1, 1),
-                                slicePartitions: 24,
-                                stackPartitions: 36,
-                            })
-                        }))
+                                entity.ellipsoid = new Cesium.EllipsoidGraphics({
+                                    radii: new Cesium.Cartesian3(150000.0, 150000.0, 150000.0),
+                                    material: new Cesium.ColorMaterialProperty(colorCallback),
+                                    slicePartitions: 24,
+                                    stackPartitions: 36,
+                                })
+                            });
+                        })
                         .catch(error => {
                             alert(error)
                         })
-                }) .catch(error => {
-                    alert(error);
-                })
+                }).catch(error => {
+                alert(error);
+            })
         }
         reader.onerror = () => {
             alert(reader.error);
--- src/js/table.js
+++ src/js/table.js
@@ -1,23 +1,27 @@
+import {viewer} from "../index";
+
 const tableObject = document.getElementById("table")
+let id = 0
+export {
+    id
+}
 
 export function addRow(filename) {
     const newRow = tableObject.insertRow();
+
     const checkboxCell = newRow.insertCell();
     checkboxCell.innerHTML =
-        "<div class=\"form-check\">\n" +
-        "    <label>\n" +
-        "        <input class=\"form-check-input\" type=\"checkbox\" value=\"\">\n" +
-        "    </label>\n" +
-        "</div>"
+        "<input class=\"form-check-input\" type=\"checkbox\" value=\"\">\n"
+
 
     const nameCell = newRow.insertCell();
     nameCell.innerHTML = filename
 
+
     const colorCell = newRow.insertCell();
     colorCell.innerHTML =
-        "<label for=\"сolorInput\" class=\"form-label\"></label>" +
-        "<input type=\"color\" class=\"form-control form-control-color\" id=\"сolorInput\" value=\"#FFFFFF\" title=\"Choose your color\" autocompleted=\"\">"
-
+        `<label for="colorInput${id}" class="form-label"></label>` +
+        `<input type="color" class="form-control form-control-color" id="colorInput${id}" value="#FFFFFF">`
 
     const imgCell = newRow.insertCell();
     imgCell.innerHTML =
@@ -25,4 +29,26 @@ export function addRow(filename) {
         "     Img\n" +
         "</label>\n" +
         "<input accept=\".svg\" id=\"file-upload\" type=\"file\"/>"
+
+    id++
 }
+
+
+const deleteItems = document.getElementById("deleteItems")
+deleteItems.addEventListener("click", () => {
+    const rowCount = tableObject.rows.length;
+    for (let i = 1; i < rowCount; ++i) {
+        const row = tableObject.rows[i];
+        const chkbox = row.cells[0].getElementsByTagName('input')[0];
+        const dataName = row.cells[1].innerHTML;
+        if ('checkbox' === chkbox.type && true === chkbox.checked) {
+            tableObject.deleteRow(i);
+            const dataSources = viewer.dataSources;
+            dataSources.getByName(dataName).forEach( (data) => {
+                dataSources.remove(data, true);
+            });
+            i--;
+            id--;
+        }
+    }
+})
