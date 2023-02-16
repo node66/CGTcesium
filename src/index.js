e42ab182c7b4dccdefff7f03a32539c593f1a654
Vitaliy Shumikhin <vitaliy.shumikhin@cgtribe.com>
2023-02-16 11:28:33 +0400

    test

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import "bootstrap";
+import 'bootstrap';
 
-import * as Cesium from "cesium";
-import {Viewer} from "cesium";
-import "cesium/Build/Cesium/Widgets/widgets.css";
-import "../src/css/main.css"
-import {addRow} from "./js/table";
-
+import * as Cesium from 'cesium';
+import {Viewer} from 'cesium';
+import 'cesium/Build/Cesium/Widgets/widgets.css';
+import '../src/css/main.css';
+import {addRow} from './js/table';
 
 export const viewer = new Viewer('cesiumContainer');
 export const scene = viewer.scene;
@@ -13,57 +12,53 @@ export const scene = viewer.scene;
 const ellipsoid = scene.globe.ellipsoid;
 
 const strToFloat = (str) => {
-    return parseFloat(str.replace(",", "."));
-}
+  return parseFloat(str.replace(',', '.'));
+};
 
-
-const fileInput = document.getElementById("geojson-file-upload");
+const fileInput = document.getElementById('geojson-file-upload');
 fileInput.onchange = () => {
-    const files = fileInput.files;
+  const files = fileInput.files;
 
-    Object.keys(files).forEach(i => {
-        const file = files[i];
-        const reader = new FileReader();
-        reader.readAsText(file);
-        reader.onload = (e) => {
-            Cesium.GeoJsonDataSource.load(JSON.parse(e.target.result))
-                .then(dataSource => {
-                    dataSource.name = file.name;
-                    const [data] = viewer.dataSources.getByName(file.name);
-                    if (viewer.dataSources.contains(data))
-                        return;
+  Object.keys(files).forEach(i => {
+    const file = files[i];
+    const reader = new FileReader();
+    reader.readAsText(file);
+    reader.onload = (e) => {
+      Cesium.GeoJsonDataSource.load(JSON.parse(e.target.result)).
+          then(dataSource => {
+            dataSource.name = file.name;
+            const [data] = viewer.dataSources.getByName(file.name);
+            if (viewer.dataSources.contains(data)) return;
 
-                    addRow(file.name);
-                    viewer.dataSources.add(dataSource)
-                        .then(r => {
-                            r.entities.values.forEach(entity => {
-                                const cartographic = Cesium.Cartographic.fromCartesian(
-                                    entity.position.getValue(Cesium.JulianDate.now())
-                                )
-                                entity.billboard = undefined; //show only points
-                                entity.point = {
-                                    pixelSize: 10,
-                                }
+            addRow(file.name);
+            viewer.dataSources.add(dataSource).then(r => {
+              r.entities.values.forEach(entity => {
+                const cartographic = Cesium.Cartographic.fromCartesian(
+                    entity.position.getValue(Cesium.JulianDate.now()));
+                entity.billboard = undefined; //show only points
+                entity.point = {
+                  pixelSize: 10,
+                };
 
-                                cartographic.height = strToFloat(String(entity.properties.Altitude));
-                                entity.position = Cesium.Cartesian3.fromDegrees(
-                                    cartographic.longitude * Cesium.Math.DEGREES_PER_RADIAN,
-                                    cartographic.latitude * Cesium.Math.DEGREES_PER_RADIAN,
-                                    cartographic.height * Cesium.Math.DEGREES_PER_RADIAN,
-                                    ellipsoid,
-                                )
-                            });
-                            viewer.zoomTo(r)
-                        })
-                        .catch(error => {
-                            alert(error)
-                        })
-                }).catch(error => {
-                alert(error);
-            })
-        }
-        reader.onerror = () => {
-            alert(reader.error);
-        }
-    });
-}
+                cartographic.height = strToFloat(
+                    String(entity.properties.Altitude));
+                entity.position = Cesium.Cartesian3.fromDegrees(
+                    cartographic.longitude * Cesium.Math.DEGREES_PER_RADIAN,
+                    cartographic.latitude * Cesium.Math.DEGREES_PER_RADIAN,
+                    cartographic.height * Cesium.Math.DEGREES_PER_RADIAN,
+                    ellipsoid);
+
+                entity.ellipsoid = {
+                  radii: new Cesium.Cartesian3(150000.0, 150000.0, 150000.0),
+                  material: Cesium.Color.RED.withAlpha(0.0),
+                };
+
+              });
+              viewer.zoomTo(r).catch(error => alert(error));
+            }).catch(error => alert(error));
+          }).
+          catch(error => alert(error));
+    };
+    reader.onerror = () => alert(reader.error);
+  });
+};
--- src/js/table.js
+++ src/js/table.js
@@ -1,178 +1,181 @@
-import {viewer} from "../index";
-import * as Cesium from "cesium";
+import {viewer} from '../index';
+import * as Cesium from 'cesium';
 
-
-const tableObject = document.getElementById("table");
-let id = 0
+const tableObject = document.getElementById('table');
+const [tableBody] = tableObject.getElementsByTagName('tbody');
+let id = 0;
 export {
-    id
-}
+  id,
+};
 
 export function addRow(filename) {
-    const newRow = tableObject.insertRow();
+  const newRow = tableBody.insertRow();
 
-    const checkboxCell = newRow.insertCell();
-    checkboxCell.innerHTML =
-        `<input checked class="form-check-input" id="check${id}" name="checkbox" type="checkbox" value="">`
+  const checkboxCell = newRow.insertCell();
+  checkboxCell.innerHTML = `<input checked class="form-check-input" id="check${id}" name="checkbox" type="checkbox" value="">`;
 
+  const checkBox = document.getElementById(`check${id}`);
+  checkBox.addEventListener('change', () => {
+    const [data] = viewer.dataSources.getByName(filename);
+    const checked = checkBox.checked;
+    data.show = checked;
+    const polyline = viewer.entities.getById(filename)?.polyline;
+    if (polyline !== undefined) if (checked) {
+      polyline.show = !!polyCheck.checked;
+    } else polyline.show = false;
+  });
 
-    const checkBox = document.getElementById(`check${id}`)
-    checkBox.addEventListener("change",
-        () => {
-            const [data] = viewer.dataSources.getByName(filename);
-            const checked = checkBox.checked
-            data.show = checked
-            const polyline = viewer.entities.getById(filename)?.polyline
-            if (polyline !== undefined)
-                if (checked) {
-                    polyline.show = !!polyCheck.checked;
-                } else
-                    polyline.show = false
-        })
-
-    const mainCheck = document.getElementById("mainСheckbox")
-    mainCheck.disabled = false;
-    mainCheck.checked = true;
-    mainCheck.addEventListener("change",
-        () => {
-            const rowCount = tableObject.rows.length;
-            for (let i = 1; i < rowCount; ++i) {
-                const row = tableObject.rows[i];
-                const [chkbox] = row.cells[0].getElementsByTagName('input');
-                chkbox.checked = mainCheck.checked
-            }
-        }
-    )
-
-
-    const nameCell = newRow.insertCell();
-    nameCell.innerHTML = filename
-
-    const sizeCell = newRow.insertCell();
-    sizeCell.innerHTML =
-        `<input id="sizeInput${id}" type="number" min="0" required value="10">`
-
-
-    const sizeInp = document.getElementById(`sizeInput${id}`);
-    sizeInp.onchange = sizeInp.onclick = sizeAndColorChange;
-
-
-    const colorCell = newRow.insertCell();
-    colorCell.innerHTML =
-        `<label for="colorInput${id}" class="form-label"></label>` +
-        `<input type="color" class="form-control form-control-color" id="colorInput${id}" value="#FFFFFF">`
-
-    const colorInp = document.getElementById(`colorInput${id}`);
-    colorInp.onclick = sizeAndColorChange;
-
-    function sizeAndColorChange() {
-        const [data] = viewer.dataSources.getByName(nameCell.innerHTML);
-        data.entities.values.forEach(entity => {
-            entity.point = {
-                pixelSize: new Cesium.CallbackProperty(
-                    () => {
-                        return sizeInp.value;
-                    }, false
-                ),
-                color: new Cesium.CallbackProperty(
-                    () => {
-                        return Cesium.Color.fromCssColorString(
-                            colorInp.value);
-                    }, false
-                ),
-            }
-        })
-    }
-
-
-    const imgCell = newRow.insertCell();
-    imgCell.innerHTML =
-        `<label for="pngFile${id}" class="form-label">
-         <canvas id="canvas${id}" width="32" height="32" style="border:1px solid #000000;">
-         </canvas><input class="form-control" accept=".png" type="file" id="pngFile${id}" >`
-
-    const imgInp = document.getElementById(`pngFile${id}`);
-    const canvas = document.getElementById(`canvas${id}`),
-        context = canvas.getContext('2d');
-
-    let path;
-    imgInp.onchange = () => {
-        const [file] = imgInp.files;
-        path = URL.createObjectURL(file);
-        const img = new Image();
-        if (file) {
-            img.onload = () => {
-                img.style.width = 32;
-                img.style.height = 32;
-                context.drawImage(img, 0, 0);
-            };
-            img.src = path;
-            const [data] = viewer.dataSources.getByName(nameCell.innerHTML);
-            data.entities.values.forEach(entity => {
-                entity.point.show = false;
-                entity.billboard = new Cesium.BillboardGraphics({
-                    image: path,
-                });
-            });
-        }
-    }
-
-
-    const poly = newRow.insertCell();
-    poly.innerHTML =
-        `<input  class="form-check-input" id="connectPoints${id}" type="checkbox" value="">`
-
-    const polyCheck = document.getElementById(`connectPoints${id}`)
-
-
-    polyCheck.addEventListener("change",
-        () => {
-            let cartesians = []
-            const [data] = viewer.dataSources.getByName(nameCell.innerHTML);
-            data.entities.values.forEach(entity => {
-                cartesians.push(entity.position.getValue(Cesium.JulianDate.now()))
-            });
-            const polyline = viewer.entities.getById(nameCell.innerHTML)?.polyline
-            if (polyline === undefined)
-                viewer.entities.add({
-                    id: nameCell.innerHTML,
-                    polyline: {
-                        positions: cartesians,
-                        arcType: Cesium.ArcType.NONE,
-                        width: 2,
-                        material: new Cesium.PolylineOutlineMaterialProperty({
-                            color: new Cesium.CallbackProperty(
-                                () => {
-                                    return Cesium.Color.fromCssColorString(
-                                        colorInp.value);
-                                }, false
-                            ),
-                        }),
-                    }
-                })
-            else
-                polyline.show = !!(polyCheck.checked && checkBox.checked);
-        })
-
-
-    id++
-}
-
-const deleteItems = document.getElementById("deleteItems")
-deleteItems.addEventListener("click", () => {
+  const mainCheck = document.getElementById('mainСheckbox');
+  mainCheck.disabled = false;
+  mainCheck.checked = true;
+  mainCheck.addEventListener('change', () => {
     const rowCount = tableObject.rows.length;
     for (let i = 1; i < rowCount; ++i) {
-        const row = tableObject.rows[i];
-        const [chkbox] = row.cells[0].getElementsByTagName('input');
-        const dataName = row.cells[1].innerHTML;
-        if ('checkbox' === chkbox.type && true === chkbox.checked) {
-            tableObject.deleteRow(i);
-            const dataSources = viewer.dataSources;
-            dataSources.getByName(dataName).forEach(data => {
-                dataSources.remove(data, true);
-            });
-            i--;
-            id--;
-        }
+      const row = tableObject.rows[i];
+      const [chkbox] = row.cells[0].getElementsByTagName('input');
+      chkbox.checked = mainCheck.checked;
     }
-})
+  });
+
+  const nameCell = newRow.insertCell();
+  nameCell.innerHTML = filename;
+
+  const imgCell = newRow.insertCell();
+  imgCell.innerHTML = `<label for="pngFile${id}" class="form-label">
+         <canvas id="canvas${id}" width="32" height="32" style="border:1px solid #000000;">
+         </canvas><input class="form-control" accept=".png" type="file" id="pngFile${id}" >`;
+
+  const imgInp = document.getElementById(`pngFile${id}`);
+  const canvas = document.getElementById(`canvas${id}`),
+      context = canvas.getContext('2d');
+
+  imgInp.onchange = () => {
+    const [file] = imgInp.files;
+    const path = URL.createObjectURL(file);
+    const imgSize = 32;
+    context.clearRect(0, 0, imgSize, imgSize);
+    const img = new Image();
+    if (file) {
+      img.onload = () => {
+        context.drawImage(img, 0, 0, imgSize, imgSize);
+      };
+      img.src = path;
+      const [data] = viewer.dataSources.getByName(nameCell.innerHTML);
+      data.entities.values.forEach(entity => {
+        entity.point.show = false;
+        entity.billboard = new Cesium.BillboardGraphics({
+          image: path, width: new Cesium.CallbackProperty(() => {
+            return Number(sizeInp.value);
+          }, false), height: new Cesium.CallbackProperty(() => {
+            return Number(sizeInp.value);
+          }, false),
+        });
+      });
+    }
+  };
+
+  const poly = newRow.insertCell();
+  poly.innerHTML =
+      `<table class="table"></table>` +
+      ` ` +
+      `<input  class="form-check-input" id="connectPoints${id}" type="checkbox" value="">`;
+
+  const polyCheck = document.getElementById(`connectPoints${id}`);
+
+  polyCheck.addEventListener('change', () => {
+    let cartesianCoord = [];
+    const [data] = viewer.dataSources.getByName(nameCell.innerHTML);
+    data.entities.values.forEach(entity => {
+      cartesianCoord.push(entity.position.getValue(Cesium.JulianDate.now()));
+    });
+    const polyline = viewer.entities.getById(nameCell.innerHTML)?.polyline;
+    if (polyline === undefined) viewer.entities.add({
+      id: nameCell.innerHTML, polyline: {
+        positions: cartesianCoord, arcType: Cesium.ArcType.NONE,
+        width: 2, //may be need to
+        material: new Cesium.PolylineOutlineMaterialProperty({
+          color: new Cesium.CallbackProperty(() => {
+            return Cesium.Color.fromCssColorString(colorInp.value);
+          }, false),
+        }),
+      },
+    }); else polyline.show = !!(polyCheck.checked && checkBox.checked);
+  });
+
+  const colorCell = newRow.insertCell();
+  colorCell.innerHTML =
+      '<table class="table table-bordered border-primary">' +
+      ' <tr>' +
+      '   <th scope="row">item</th>' +
+      '   <td colspan="2"><label for="colorInput${id}" class="form-label"></label>' +
+      `      <input type="color" class="form-control form-control-color" id="colorInput${id}" value="#FFFFFF">` +
+      '</td>' +
+      ' </tr>' +
+      ' <tr>' +
+      '   <th scope="row">3d buffer</th>' +
+      '   <td><label for="colorInput${id}" class="form-label"></label>' +
+      `      <input type="color" class="form-control form-control-color" id="colorInput${id}" value="#FFFFFF">` +
+      '</td>' +
+      ' </tr>' +
+      '</table>';
+
+  const colorInp = document.getElementById(`colorInput${id}`);
+  colorInp.onclick = sizeAndColorChange;
+
+  function sizeAndColorChange() {
+    const [data] = viewer.dataSources.getByName(nameCell.innerHTML);
+    const showPoint = data.entities.values[0].billboard === undefined;
+    data.entities.values.forEach(entity => {
+      if (showPoint) {
+        entity.point = {
+          pixelSize: new Cesium.CallbackProperty(() => {
+            return sizeInp.value;
+          }, false), color: new Cesium.CallbackProperty(() => {
+            return Cesium.Color.fromCssColorString(colorInp.value);
+          }, false), show: true,
+        };
+      } else if (!showPoint) {
+        entity.point.show = false;
+        entity.billboard = {
+          width: new Cesium.CallbackProperty(() => {
+            return Number(sizeInp.value);
+          }, false),
+          height: new Cesium.CallbackProperty(() => {
+            return Number(sizeInp.value);
+          }, false),
+          color: new Cesium.CallbackProperty(() => {
+            return Cesium.Color.fromCssColorString(colorInp.value);
+          }, false),
+          show: true,
+        };
+      }
+    });
+  }
+
+  const sizeCell = newRow.insertCell();
+  sizeCell.innerHTML = `<input id="sizeInput${id}" type="number" min="0" required value="10">`;
+
+  const sizeInp = document.getElementById(`sizeInput${id}`);
+  sizeInp.onchange = sizeInp.onclick = sizeAndColorChange;
+
+  id++;
+}
+
+const deleteItems = document.getElementById('deleteItems');
+deleteItems.addEventListener('click', () => {
+  const rowCount = tableObject.rows.length;
+  for (let i = 1; i < rowCount; ++i) {
+    const row = tableObject.rows[i];
+    const [chkbox] = row.cells[0].getElementsByTagName('input');
+    const dataName = row.cells[1].innerHTML;
+    if ('checkbox' === chkbox.type && true === chkbox.checked) {
+      tableObject.deleteRow(i);
+      const dataSources = viewer.dataSources;
+      dataSources.getByName(dataName).forEach(data => {
+        dataSources.remove(data, true);
+      });
+      i--;
+      id--;
+    }
+  }
+});
